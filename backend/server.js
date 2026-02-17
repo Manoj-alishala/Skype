4821ec22313cb85f03e8144c220713871534bad3
Manoj <alishalamanoj56@gmail.com>
2026-02-17 19:41:42 +0530

    fix: use Express 5 wildcard syntax

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.use("/api/users", userRoutes);
 
 app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-app.get("*", (req, res) => {
+app.get("/{*splat}", (req, res) => {
 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
