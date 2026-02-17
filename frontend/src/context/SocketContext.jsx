7d5781dacd52e89e84807576b576311f757768c7
Manoj <alishalamanoj56@gmail.com>
2026-02-17 10:59:27 +0530

    backend connection

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://chat-application-ba.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
